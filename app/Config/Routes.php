34b578ecbe2a8d49f18b4b390a2db4f73a3b5639
markpijera <jinwosung490@gmail.com>
2025-11-02 22:49:26 +0800

    Add Medicine API controller and configure API routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,30 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// API Routes
+$routes->group('api/v1', ['namespace' => 'App\Controllers\Api'], function($routes) {
+    // Patient routes
+    $routes->get('patients', 'PatientController::index');
+    $routes->get('patients/(:num)', 'PatientController::show/$1');
+    $routes->post('patients', 'PatientController::create');
+    $routes->put('patients/(:num)', 'PatientController::update/$1');
+    $routes->delete('patients/(:num)', 'PatientController::delete/$1');
+
+    // Appointment routes
+    $routes->get('appointments', 'AppointmentController::index');
+    $routes->post('appointments', 'AppointmentController::create');
+    $routes->put('appointments/(:num)', 'AppointmentController::update/$1');
+    $routes->post('appointments/(:num)/confirm', 'AppointmentController::confirm/$1');
+    $routes->post('appointments/(:num)/cancel', 'AppointmentController::cancel/$1');
+    $routes->delete('appointments/(:num)', 'AppointmentController::delete/$1');
+
+    // Medicine routes
+    $routes->get('medicines', 'MedicineController::index');
+    $routes->get('medicines/alerts', 'MedicineController::alerts');
+    $routes->get('medicines/(:num)', 'MedicineController::show/$1');
+    $routes->post('medicines', 'MedicineController::create');
+    $routes->put('medicines/(:num)', 'MedicineController::update/$1');
+    $routes->post('medicines/(:num)/dispense', 'MedicineController::dispense/$1');
+    $routes->delete('medicines/(:num)', 'MedicineController::delete/$1');
+});
